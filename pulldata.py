464ef7ddff20a96c147e3c77ac76cd959bb786c1
cjallen141 <christopher.allen@outlook.com>
2016-12-22 19:55:14 -0800

    successfully scraping stevens forecast data from weather.gov

--- pulldata.py
+++ pulldata.py
@@ -1,5 +1,6 @@
 
 import urllib2, datetime, re, json, os
+from bs4 import BeautifulSoup
 
 
 
@@ -13,7 +14,7 @@ stevens_west_cams = {"W Stevens Pass" : 9145,
 		"E Stevens Summit": 8062}
 
 
-def getTravelerInfoJson(type):
+def get_traveler_info_json(type):
 	url = urls[type] + "?AccessCode=" + access_code
 
 	f = urllib2.urlopen(url)
@@ -32,19 +33,59 @@ def getTravelerInfoJson(type):
 def get_latest_camera_pic(cam_list):
 
 	path = os.getcwd()
-	print path
+	#print path
 	for cam in cam_list:
-		print cam
+		#print cam
 		f_path = path + "\\" + str(cam["CameraID"]) + ".jpg"
-		print f_path
+		print "downloading camera: " + cam["ImageURL"]
 		img = urllib2.urlopen(cam["ImageURL"])
 
 		with open(f_path, 'wb') as localFile:
 			localFile.write(img.read())
-
 	return
+
+def get_stevens_forecast_data_update():
+
+	#http request current weather.gov forecast data and create soup object
+	url = "http://forecast.weather.gov/MapClick.php?lat=47.75&lon=-121.09#.WFxdDlwWzif"
+	f = urllib2.urlopen(url)
+	soup = BeautifulSoup(f.read(), "html.parser")
+
+
+	#search for the "detailed forecast", which contains the updated forecast text 
+	forecast_section = soup.find(id="detailed-forecast")
+	#print forecast_section.prettify()
+	#print '\n'
+
+
+	#create list of tuple forcast [('day', 'forecast') ... ]
+
+	#get forecast headers
+	forecast_headers=[]
+	for forecast in forecast_section.find_all(attrs={'class': "col-sm-2 forecast-label"}):
+		forecast_headers.append(str(forecast.string))
+
+	#get forecast content
+	forecast_text = [] 
+	for forecast in forecast_section.find_all(attrs={'class': "col-sm-10 forecast-text"}):
+		forecast_text.append(str(forecast.string))
+	#print forecast_section.prettify()
+
+	#combine into list of tuples
+	forecast = []
+	idx =0
+	for header in forecast_headers:
+		forecast.append( (header,forecast_text[idx]))
+		idx+= 1
+
+	for item in forecast:
+		print item
+	return
+
 if __name__ == '__main__':
 	
-	data = getTravelerInfoJson("HighwayCams")
+	data = get_traveler_info_json("HighwayCams")
 
-	get_latest_camera_pic(data)
+	#get_latest_camera_pic(data)
+
+	get_stevens_forecast_data_update()
